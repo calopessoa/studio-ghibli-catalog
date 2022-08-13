fc5845c4c05d70c4718d1311eb298afb628f6de7
Carlos <nashlasterby@gmail.com>
2022-08-12 22:34:14 -0300

    added seed script to database

---
+++ backend/src/prisma/data.ts
@@ -0,0 +1,373 @@
+export const ghibliMoviesOnProducers = [
+  {
+    movieId: "11e51e86-5b40-4662-9302-3fe40ac3d2df",
+    producerId: "3b37ee97-c1f3-41e7-a523-0b2e3ce54a25",
+  },
+  {
+    movieId: "1ca0c143-9d8b-47f3-b977-3bace35390b5",
+    producerId: "3b37ee97-c1f3-41e7-a523-0b2e3ce54a25",
+  },
+  {
+    movieId: "1ca0c143-9d8b-47f3-b977-3bace35390b5",
+    producerId: "568734d9-ee4c-49c4-8e9d-bc9f9a8b9774",
+  },
+  {
+    movieId: "1ca0c143-9d8b-47f3-b977-3bace35390b5",
+    producerId: "ad13001e-755e-48ef-b0f1-fe22b5c8e0b1",
+  },
+  {
+    movieId: "1ca0c143-9d8b-47f3-b977-3bace35390b5",
+    producerId: "cb597fb4-ac58-4214-8645-ab6e9b646311",
+  },
+  {
+    movieId: "1ca0c143-9d8b-47f3-b977-3bace35390b5",
+    producerId: "0b3a9393-f3da-4758-a84b-70877749099d",
+  },
+  {
+    movieId: "1d71a2ac-ca39-4bcc-9d70-e3f4fc109c25",
+    producerId: "3b37ee97-c1f3-41e7-a523-0b2e3ce54a25",
+  },
+  {
+    movieId: "2bd5ba59-f268-4725-b4a4-48106ca137d4",
+    producerId: "3b37ee97-c1f3-41e7-a523-0b2e3ce54a25",
+  },
+  {
+    movieId: "2c872196-9eb1-4b7e-a2d4-aa099471797a",
+    producerId: "0b684165-8ab2-49d0-bca6-7a3a33c8ad94",
+  },
+  {
+    movieId: "2ea2fe98-8e19-4418-a7fa-a382087014f8",
+    producerId: "3b37ee97-c1f3-41e7-a523-0b2e3ce54a25",
+  },
+  {
+    movieId: "32b04150-882c-43bb-acb8-c45d486482a2",
+    producerId: "3b37ee97-c1f3-41e7-a523-0b2e3ce54a25",
+  },
+  {
+    movieId: "34c5f0f7-7aa7-42b8-8055-71f4bb582dc1",
+    producerId: "3b37ee97-c1f3-41e7-a523-0b2e3ce54a25",
+  },
+  {
+    movieId: "36e2e5f4-41e6-41b1-814e-2a936db891e3",
+    producerId: "3b37ee97-c1f3-41e7-a523-0b2e3ce54a25",
+  },
+  {
+    movieId: "5a1b2b68-52d4-40e1-b110-06b6f4aa183b",
+    producerId: "3b37ee97-c1f3-41e7-a523-0b2e3ce54a25",
+  },
+  {
+    movieId: "6ac92872-8dc8-4928-ba5f-09b62f726b51",
+    producerId: "3b37ee97-c1f3-41e7-a523-0b2e3ce54a25",
+  },
+  {
+    movieId: "76f10223-d187-4293-b9c3-279474a77bdb",
+    producerId: "568734d9-ee4c-49c4-8e9d-bc9f9a8b9774",
+  },
+  {
+    movieId: "7ba25e8d-386b-44ca-be7e-749a8630c0f3",
+    producerId: "0b684165-8ab2-49d0-bca6-7a3a33c8ad94",
+  },
+  {
+    movieId: "81c4fbd2-ad24-4e01-9882-3c1f3342fa82",
+    producerId: "3b37ee97-c1f3-41e7-a523-0b2e3ce54a25",
+  },
+  {
+    movieId: "86471a78-d07e-4f75-9c09-4422dcce0d44",
+    producerId: "3b37ee97-c1f3-41e7-a523-0b2e3ce54a25",
+  },
+  {
+    movieId: "96739673-b4f7-4d5b-af28-8e5cccb097ab",
+    producerId: "3b37ee97-c1f3-41e7-a523-0b2e3ce54a25",
+  },
+  {
+    movieId: "99587f01-551a-4fc6-a04e-e24628f50979",
+    producerId: "3b37ee97-c1f3-41e7-a523-0b2e3ce54a25",
+  },
+  {
+    movieId: "b543b628-7ef8-4b25-b86e-564352e9500d",
+    producerId: "3b37ee97-c1f3-41e7-a523-0b2e3ce54a25",
+  },
+  {
+    movieId: "b833a713-a41d-4b53-8699-b548434d88de",
+    producerId: "07baf682-d10a-4674-8b9f-52d669c4e0b7",
+  },
+  {
+    movieId: "c7a21b27-7459-4584-82d5-4ce8b14e813c",
+    producerId: "3b37ee97-c1f3-41e7-a523-0b2e3ce54a25",
+  },
+  {
+    movieId: "cd315907-8b4d-4499-9549-5293536cd907",
+    producerId: "332d9834-e86c-40f0-8008-5794f72eb77f",
+  },
+  {
+    movieId: "f28a3620-80e9-4a36-9bb4-36958c061f9f",
+    producerId: "07baf682-d10a-4674-8b9f-52d669c4e0b7",
+  },
+];
+
+export const ghibliProducers = [
+  {
+    id: "568734d9-ee4c-49c4-8e9d-bc9f9a8b9774",
+    producer: "Isao Takahata",
+  },
+  {
+    id: "332d9834-e86c-40f0-8008-5794f72eb77f",
+    producer: "Toru Hara",
+  },
+  {
+    id: "07baf682-d10a-4674-8b9f-52d669c4e0b7",
+    producer: "Hayao Miyazaki",
+  },
+  {
+    id: "3b37ee97-c1f3-41e7-a523-0b2e3ce54a25",
+    producer: "Toshio Suzuki",
+  },
+  {
+    id: "0b684165-8ab2-49d0-bca6-7a3a33c8ad94",
+    producer: "Yoshiaki Nishimura",
+  },
+  {
+    id: "ad13001e-755e-48ef-b0f1-fe22b5c8e0b1",
+    producer: "Vincent Maraval",
+  },
+  {
+    id: "cb597fb4-ac58-4214-8645-ab6e9b646311",
+    producer: "Pascal Caucheteux",
+  },
+  {
+    id: "0b3a9393-f3da-4758-a84b-70877749099d",
+    producer: "Grégoire Sorlat",
+  },
+];
+
+export const ghibliDirectors = [
+  {
+    id: "73ae1eca-2d86-4a79-b18a-3fdcaba05b64",
+    director: "Hayao Miyazaki",
+  },
+  {
+    id: "29cbe168-4be9-4694-b6e6-3d3730df0785",
+    director: "Isao Takahata",
+  },
+  {
+    id: "f252a4cd-22ee-4e0f-a80f-bb320bdf0f0f",
+    director: "Yoshifumi Kondō",
+  },
+  {
+    id: "d6653585-8cc8-4197-af4b-7b2880ede859",
+    director: "Hiroyuki Morita",
+  },
+  {
+    id: "23fa56d4-2575-475a-8111-fa54c4ea73e3",
+    director: "Gorō Miyazaki",
+  },
+  {
+    id: "bfeec642-e034-46b2-818e-4f3bc104d4e4",
+    director: "Hiromasa Yonebayashi",
+  },
+  {
+    id: "6e4f190f-cecb-49e9-977f-02b333471621",
+    director: "Michaël Dudok de Wit",
+  },
+];
+
+export const ghibliMovies = [
+  {
+    id: "76f10223-d187-4293-b9c3-279474a77bdb",
+    title: "Castle in the Sky",
+    movie_banner:
+      "https://image.tmdb.org/t/p/w533_and_h300_bestv2/3cyjYtLWCBE1uvWINHFsFnE8LUK.jpg",
+    description:
+      "The orphan Sheeta inherited a mysterious crystal that links her to the mythical sky-kingdom of Laputa. With the help of resourceful Pazu and a rollicking band of sky pirates, she makes her way to the ruins of the once-great civilization. Sheeta and Pazu must outwit the evil Muska, who plans to use Laputa's science to make himself ruler of the world.",
+    directorId: "73ae1eca-2d86-4a79-b18a-3fdcaba05b64",
+  },
+  {
+    id: "cd315907-8b4d-4499-9549-5293536cd907",
+    title: "Grave of the Fireflies",
+    movie_banner:
+      "https://image.tmdb.org/t/p/original/vkZSd0Lp8iCVBGpFH9L7LzLusjS.jpg",
+    description:
+      "In the latter part of World War II, a boy and his sister, orphaned when their mother is killed in the firebombing of Tokyo, are left to survive on their own in what remains of civilian life in Japan. The plot follows this boy and his sister as they do their best to survive in the Japanese countryside, battling hunger, prejudice, and pride in their own quiet, personal battle.",
+    directorId: "29cbe168-4be9-4694-b6e6-3d3730df0785",
+  },
+  {
+    id: "f28a3620-80e9-4a36-9bb4-36958c061f9f",
+    title: "My Neighbor Totoro",
+    movie_banner:
+      "https://image.tmdb.org/t/p/original/etqr6fOOCXQOgwrQXaKwenTSuzx.jpg",
+    description:
+      "Two sisters move to the country with their father in order to be closer to their hospitalized mother, and discover the surrounding trees are inhabited by Totoros, magical spirits of the forest. When the youngest runs away from home, the older sister seeks help from the spirits to find her.",
+    directorId: "73ae1eca-2d86-4a79-b18a-3fdcaba05b64",
+  },
+  {
+    id: "b833a713-a41d-4b53-8699-b548434d88de",
+    title: "Kiki's Delivery Service",
+    movie_banner:
+      "https://image.tmdb.org/t/p/original/h5pAEVma835u8xoE60kmLVopLct.jpg",
+    description:
+      "A young witch, on her mandatory year of independent life, finds fitting into a new community difficult while she supports herself by running an air courier service.",
+    directorId: "73ae1eca-2d86-4a79-b18a-3fdcaba05b64",
+  },
+  {
+    id: "11e51e86-5b40-4662-9302-3fe40ac3d2df",
+    title: "Only Yesterday",
+    movie_banner:
+      "https://image.tmdb.org/t/p/w533_and_h300_bestv2/isCrlWWI4JrdLKAUAwFb5cjAsH4.jpg",
+    description:
+      "It’s 1982, and Taeko is 27 years old, unmarried, and has lived her whole life in Tokyo. She decides to visit her family in the countryside, and as the train travels through the night, memories flood back of her younger years: the first immature stirrings of romance, the onset of puberty, and the frustrations of math and boys. At the station she is met by young farmer Toshio, and the encounters with him begin to reconnect her to forgotten longings. In lyrical switches between the present and the past, Taeko contemplates the arc of her life, and wonders if she has been true to the dreams of her childhood self.",
+    directorId: "29cbe168-4be9-4694-b6e6-3d3730df0785",
+  },
+  {
+    id: "c7a21b27-7459-4584-82d5-4ce8b14e813c",
+    title: "Porco Rosso",
+    movie_banner:
+      "https://image.tmdb.org/t/p/original/nAeCzilMRXvGaxiCpv63ZRVRVgh.jpg",
+    description:
+      "Porco Rosso, known in Japan as Crimson Pig (Kurenai no Buta) is the sixth animated film by Hayao Miyazaki and released in 1992. You're introduced to an Italian World War I fighter ace, now living as a freelance bounty hunter chasing 'air pirates' in the Adriatic Sea. He has been given a curse that changed his head to that of a pig. Once called Marco Pagot, he is now known to the world as 'Porco Rosso', Italian for 'Red Pig.'",
+    directorId: "73ae1eca-2d86-4a79-b18a-3fdcaba05b64",
+  },
+  {
+    id: "2ea2fe98-8e19-4418-a7fa-a382087014f8",
+    title: "Pom Poko",
+    movie_banner:
+      "https://image.tmdb.org/t/p/original/jScPd0u0jeo66l8gwDl7W9hDUnM.jpg",
+    description:
+      "As the human city development encroaches on the raccoon population's forest and meadow habitat, the raccoons find themselves faced with the very real possibility of extinction. In response, the raccoons engage in a desperate struggle to stop the construction and preserve their home.",
+    directorId: "29cbe168-4be9-4694-b6e6-3d3730df0785",
+  },
+  {
+    id: "96739673-b4f7-4d5b-af28-8e5cccb097ab",
+    title: "Whisper of the Heart",
+    movie_banner:
+      "https://image.tmdb.org/t/p/original/fRtaDgmj0CirvqFUG1XN48BDY1l.jpg",
+    description:
+      "Shizuku lives a simple life, dominated by her love for stories and writing. One day she notices that all the library books she has have been previously checked out by the same person: 'Seiji Amasawa'. Curious as to who he is, Shizuku meets a boy her age whom she finds infuriating, but discovers to her shock that he is her 'Prince of Books'. As she grows closer to him, she realises that he merely read all those books to bring himself closer to her. The boy Seiji aspires to be a violin maker in Italy, and it is his dreams that make Shizuku realise that she has no clear path for her life. Knowing that her strength lies in writing, she tests her talents by writing a story about Baron, a cat statuette belonging to Seiji's grandfather.",
+    directorId: "f252a4cd-22ee-4e0f-a80f-bb320bdf0f0f",
+  },
+  {
+    id: "81c4fbd2-ad24-4e01-9882-3c1f3342fa82",
+    title: "Princess Mononoke",
+    movie_banner:
+      "https://image.tmdb.org/t/p/original/6pTqSq0zYIWCsucJys8q5L92kUY.jpg",
+    description:
+      "Ashitaka, a prince of the disappearing Ainu tribe, is cursed by a demonized boar god and must journey to the west to find a cure. Along the way, he encounters San, a young human woman fighting to protect the forest, and Lady Eboshi, who is trying to destroy it. Ashitaka must find a way to bring balance to this conflict.",
+    directorId: "73ae1eca-2d86-4a79-b18a-3fdcaba05b64",
+  },
+  {
+    id: "1d71a2ac-ca39-4bcc-9d70-e3f4fc109c25",
+    title: "My Neighbors the Yamadas",
+    movie_banner:
+      "https://image.tmdb.org/t/p/original/nDOsicEg4RHDq0t23JKGSb58z6u.jpg",
+    description:
+      "The Yamadas are a typical middle class Japanese family in urban Tokyo and this film shows us a variety of episodes of their lives. With tales that range from the humourous to the heartbreaking, we see this family cope with life's little conflicts, problems and joys in their own way.",
+    directorId: "29cbe168-4be9-4694-b6e6-3d3730df0785",
+  },
+  {
+    id: "6ac92872-8dc8-4928-ba5f-09b62f726b51",
+    title: "Spirited Away",
+    movie_banner:
+      "https://image.tmdb.org/t/p/original/bSXfU4dwZyBA1vMmXvejdRXBvuF.jpg",
+    description:
+      "Spirited Away is an Oscar winning Japanese animated film about a ten year old girl who wanders away from her parents along a path that leads to a world ruled by strange and unusual monster-like animals. Her parents have been changed into pigs along with others inside a bathhouse full of these creatures. Will she ever see the world how it once was?",
+    directorId: "73ae1eca-2d86-4a79-b18a-3fdcaba05b64",
+  },
+  {
+    id: "2bd5ba59-f268-4725-b4a4-48106ca137d4",
+    title: "The Cat Returns",
+    movie_banner:
+      "https://image.tmdb.org/t/p/original/d4BTZvckFTthyhGX27LZnWxl0tl.jpg",
+    description:
+      "Haru, a schoolgirl bored by her ordinary routine, saves the life of an unusual cat and suddenly her world is transformed beyond anything she ever imagined. The Cat King rewards her good deed with a flurry of presents, including a very shocking proposal of marriage to his son! Haru embarks on an unexpected journey to the Kingdom of Cats where her eyes are opened to a whole other world.",
+    directorId: "d6653585-8cc8-4197-af4b-7b2880ede859",
+  },
+  {
+    id: "99587f01-551a-4fc6-a04e-e24628f50979",
+    title: "Howl's Moving Castle",
+    movie_banner:
+      "https://image.tmdb.org/t/p/original/hjlvbMKhQm7N8tYynr8yQ8GBmqe.jpg",
+    description:
+      "When Sophie, a shy young woman, is cursed with an old body by a spiteful witch, her only chance of breaking the spell lies with a self-indulgent yet insecure young wizard and his companions in his legged, walking home.",
+    directorId: "73ae1eca-2d86-4a79-b18a-3fdcaba05b64",
+  },
+  {
+    id: "86471a78-d07e-4f75-9c09-4422dcce0d44",
+    title: "Tales from Earthsea",
+    movie_banner:
+      "https://image.tmdb.org/t/p/original/j276noIGGmfi66EnCfewsL2OVTX.jpg",
+    description:
+      "Something bizarre has come over the land. The kingdom is deteriorating. People are beginning to act strange... What's even more strange is that people are beginning to see dragons, which shouldn't enter the world of humans. Due to all these bizarre events, Ged, a wandering wizard, is investigating the cause. During his journey, he meets Prince Arren, a young distraught teenage boy. While Arren may look like a shy young teen, he has a severe dark side, which grants him strength, hatred, ruthlessness and has no mercy, especially when it comes to protecting Teru. For the witch Kumo this is a perfect opportunity. She can use the boy's 'fears' against the very one who would help him, Ged.",
+    directorId: "23fa56d4-2575-475a-8111-fa54c4ea73e3",
+  },
+  {
+    id: "5a1b2b68-52d4-40e1-b110-06b6f4aa183b",
+    title: "Ponyo",
+    movie_banner:
+      "https://image.tmdb.org/t/p/original/6a1qZ1qat26mAIK3Lq8iYdGpyHm.jpg",
+    description:
+      "The son of a sailor, 5-year old Sosuke lives a quiet life on an oceanside cliff with his mother Lisa. One fateful day, he finds a beautiful goldfish trapped in a bottle on the beach and upon rescuing her, names her Ponyo. But she is no ordinary goldfish. The daughter of a masterful wizard and a sea goddess, Ponyo uses her father's magic to transform herself into a young girl and quickly falls in love with Sosuke, but the use of such powerful sorcery causes a dangerous imbalance in the world. As the moon steadily draws nearer to the earth and Ponyo's father sends the ocean's mighty waves to find his daughter, the two children embark on an adventure of a lifetime to save the world and fulfill Ponyo's dreams of becoming human.",
+    directorId: "73ae1eca-2d86-4a79-b18a-3fdcaba05b64",
+  },
+  {
+    id: "36e2e5f4-41e6-41b1-814e-2a936db891e3",
+    title: "Arrietty",
+    movie_banner:
+      "https://image.tmdb.org/t/p/original/7Z7WVzJsSReG8B0CaPk0bvWD7tK.jpg",
+    description:
+      "14-year-old Arrietty and the rest of the Clock family live in peaceful anonymity as they make their own home from items 'borrowed' from the house's human inhabitants. However, life changes for the Clocks when a human boy discovers Arrietty.",
+    directorId: "bfeec642-e034-46b2-818e-4f3bc104d4e4",
+  },
+  {
+    id: "34c5f0f7-7aa7-42b8-8055-71f4bb582dc1",
+    title: "From Up on Poppy Hill",
+    movie_banner:
+      "https://image.tmdb.org/t/p/original/xtPBZYaWQMQxRpy7mkdk5n1bTxs.jpg",
+    description:
+      "The story is set in 1963 in Yokohama. Kokuriko Manor sits on a hill overlooking the harbour. A 16 year-old girl, Umi, lives in that house. Every morning she raises a signal flag facing the sea. The flag means “I pray for safe voyages”. A 17 year-old boy, Shun, always sees this flag from the sea as he rides a tugboat to school. Gradually the pair are drawn to each other but they are faced with a sudden trial. Even so, they keep going without running from facing the hardships of reality.",
+    directorId: "23fa56d4-2575-475a-8111-fa54c4ea73e3",
+  },
+  {
+    id: "32b04150-882c-43bb-acb8-c45d486482a2",
+    title: "The Wind Rises",
+    movie_banner:
+      "https://image.tmdb.org/t/p/original/stM3jlD4nSJhlvR2DE7XnB0eN25.jpg",
+    description:
+      "A lifelong love of flight inspires Japanese aviation engineer Jiro Horikoshi, whose storied career includes the creation of the A-6M World War II fighter plane.",
+    directorId: "73ae1eca-2d86-4a79-b18a-3fdcaba05b64",
+  },
+  {
+    id: "2c872196-9eb1-4b7e-a2d4-aa099471797a",
+    title: "The Tale of the Princess Kaguya",
+    movie_banner:
+      "https://image.tmdb.org/t/p/original/lMaWlYThCSnsmW3usxWTpSuyZp1.jpg",
+    description:
+      "A bamboo cutter named Sanuki no Miyatsuko discovers a miniature girl inside a glowing bamboo shoot. Believing her to be a divine presence, he and his wife decide to raise her as their own, calling her 'Princess'.",
+    directorId: "29cbe168-4be9-4694-b6e6-3d3730df0785",
+  },
+  {
+    id: "7ba25e8d-386b-44ca-be7e-749a8630c0f3",
+    title: "When Marnie Was There",
+    movie_banner:
+      "https://image.tmdb.org/t/p/original/axUX7urQDwCGQ9qbgh2Yys7qY9J.jpg",
+    description:
+      "The film follows Anna Sasaki living with her relatives in the seaside town. Anna comes across a nearby abandoned mansion, where she meets Marnie, a mysterious girl who asks her to promise to keep their secrets from everyone. As the summer progresses, Anna spends more time with Marnie, and eventually Anna learns the truth about her family and foster care.",
+    directorId: "bfeec642-e034-46b2-818e-4f3bc104d4e4",
+  },
+  {
+    id: "1ca0c143-9d8b-47f3-b977-3bace35390b5",
+    title: "The Red Turtle",
+    movie_banner:
+      "https://image.tmdb.org/t/p/original/kjXdW5H3myRBmTMYgKayjphr2FA.jpg",
+    description:
+      "A man set adrift by a storm wakes up on a beach. He discovers that he is on a deserted island with plenty of fresh water, fruit and a dense bamboo forest. He builds a raft from bamboo and attempts to sail away, but his raft is destroyed by an unseen monster in the sea, forcing him back to the island. He tries again with another, larger raft, but is again foiled by the creature. A third attempt again ends with the raft destroyed, but this time he is confronted by a giant red turtle, which stares at him, and forces him back to the island.",
+    directorId: "6e4f190f-cecb-49e9-977f-02b333471621",
+  },
+  {
+    id: "b543b628-7ef8-4b25-b86e-564352e9500d",
+    title: "Earwig and the Witch",
+    movie_banner:
+      "https://www.themoviedb.org/t/p/original/qMxpGzmmnY1jLd4p7EhhoW43wWF.jpg",
+    description:
+      "An orphan girl, Earwig, is adopted by a witch and comes home to a spooky house filled with mystery and magic.",
+    directorId: "23fa56d4-2575-475a-8111-fa54c4ea73e3",
+  },
+];
