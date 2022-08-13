cce4d2c17368564ac90064d37b40598bf1c593a0
Carlos <nashlasterby@gmail.com>
2022-08-13 17:24:07 -0300

    added react-TS template and the frontend structure

---
+++ frontend/src/App.tsx
@@ -0,0 +1,13 @@
+function App() {
+  return (
+    <div className="App">
+      <header>
+        <h1>
+          <div className="title">Films Catalog</div>
+        </h1>
+      </header>
+    </div>
+  );
+}
+
+export default App;
