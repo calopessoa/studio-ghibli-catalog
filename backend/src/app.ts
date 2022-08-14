3fff8c7743b2c029fd951c3b465ce655499c1ccd
Carlos <nashlasterby@gmail.com>
2022-08-14 02:07:20 -0300

    installed dependency cors

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -2,10 +2,12 @@ import "express-async-errors";
 import express from "express";
 import { routes } from "./routes";
 import { GlobalError } from "./middlewares";
+import cors from "cors";
 
 export const app = express();
 
 app.use(express.json());
+app.use(cors());
 app.use(routes);
 
 app.use(GlobalError);
