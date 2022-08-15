015a62118f6baeb6328a8c9244d30131bc2a6499
Carlos <nashlasterby@gmail.com>
2022-08-15 00:15:06 -0300

    changed stylization in header

---
+++ frontend/src/components/Footer.tsx
@@ -0,0 +1,8 @@
+export const Footer: React.FC = () => {
+
+  return (
+    <footer>
+      <h3>{ }</h3>
+    </footer>
+  )
+}
--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -1,33 +1,10 @@
-// import { usePage, Page } from '../contexts/Page';
-
 export const Header: React.FC = () => {
-  // const { page, setPage } = usePage();
-
-  // const activeButton = (flashButton: Page) => {
-  //   return (
-  //     <button
-  //       className={flashButton === page ? 'active' : ''}
-  //       onClick={() => setPage(flashButton)}
-  //     >
-  //       {flashButton}
-  //     </button>
-  //   )
-  // }
 
   return (
     <header>
-    <h1>
-      <div className="title">Studio Ghibli Animation Films Catalog</div>
-    </h1>
-    {/* <h3>
-      <div className='title'>sort by</div>
-    </h3>
-      <div className='navigation'>
-        <div className='internal'>
-          {activeButton(Page.All)}
-          {activeButton(Page.Directors)}
-        </div>
-    </div> */}
+      <h1>
+        Studio Ghibli Animation Films Catalog
+      </h1>
   </header>
   )
 }
