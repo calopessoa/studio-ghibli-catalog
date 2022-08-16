2033a67577a549802c0ff4ae1fb8b2003ff6addf
Carlos <nashlasterby@gmail.com>
2022-08-16 12:56:12 -0300

    moved from MySQL to Postgres

--- backend/src/prisma/migrations/20220812231526_added_banner_prop_to_movie_table/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - Added the required column `banner` to the `movies` table without a default value. This is not possible if the table is not empty.
-
-*/
--- AlterTable
-ALTER TABLE `movies` ADD COLUMN `banner` VARCHAR(191) NOT NULL;
--- backend/src/prisma/migrations/20220813003158_added_long_text_prop_to_movie_table/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - Made the column `description` on table `movies` required. This step will fail if there are existing NULL values in that column.
-
-*/
--- AlterTable
-ALTER TABLE `movies` MODIFY `description` LONGTEXT NOT NULL;
